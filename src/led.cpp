e589824c546e1e27dfa71c9a634511fb14e2b43a
elbriga <gabrielortizlour@gmail.com>
2026-08-27 16:34:04 -0300

    refactor

--- src/led.cpp
+++ src/led.cpp
@@ -1,26 +1,52 @@
 #include <sys/time.h>
 
 #include "eTomadaLite.h"
+#include "loga.h"
+#include "led.h"
+#include "hardwareProfile.h"
+
+// Função de log para esta modulo
+#define logaM(nivel, fmt, ...) loga("LED", nivel, fmt, ##__VA_ARGS__)
+
+extern const HardwareProfile hardwareProfile;
 
 bool ledState;
 
 void ledInit()
 {
-  pinMode(LED_GPIO, OUTPUT);   // Led Azul do MINI
-  digitalWrite(LED_GPIO, LOW); // LOW = Aceso!
+  if (!ledAtivo())
+    return;
+
+  logaM(LOG_NORMAL, "Ativando led de status em [%d]", hardwareProfile.ledPin);
+  pinMode(hardwareProfile.ledPin, OUTPUT);                             // Led Azul do MINI
+  digitalWrite(hardwareProfile.ledPin, !hardwareProfile.ledInvertido); // No MINI LOW = Aceso!
+
+  // if (hardwareProfile.ledPin == RGB_LED_PIN)
+  // {
+  //   rgbLedInit();
+  //   rgbLedSetAnim(1); // Azul == Boot!
+  // }
+}
+
+bool ledAtivo()
+{
+  return hardwareProfile.ledPin != 255;
 }
 
 void ledProcessa()
 {
+  if (!ledAtivo())
+    return;
+
   // Sincronizado com o segundo!
   struct timeval tv;
   gettimeofday(&tv, nullptr);
 
-  bool estado = tv.tv_usec > 100000;
+  bool estado = tv.tv_usec < 100000; // && !((tv.tv_usec % 200000) % 2);
 
   if (estado != ledState)
   {
     ledState = estado;
-    digitalWrite(LED_GPIO, ledState);
+    digitalWrite(hardwareProfile.ledPin, hardwareProfile.ledInvertido ? !ledState : ledState);
   }
 }
